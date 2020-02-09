87231a0426b25bc5dcf58f60a4c9e16bc7dd9e0d
Wesley Pyburn (TechNobo) <technobo@tcno.co>
2020-02-09 21:07:23 +0200

    2020-02-09

--- Python/Files-MoveToFolders.py
+++ Python/Files-MoveToFolders.py
@@ -1,5 +1,5 @@
-# Reverses filenames in folder
-# Example: Turns "12-02-2020.json" into "2020-02-12.json"
+# Moves files ending in "_0", "_1" and "_2" into folders "0", "1" and "2" respectively.
+# (My transcoded video goes into: "video.mp4", "audio_0.aac", ".._1.aac" and ".._2.aac". Having it in folders makes adding them to the Premiere Pro timeline easier, drag entire folders in.)
 # Created 2020/02/09
 
 # Created by Wesley Pyburn (TechNobo)
@@ -8,8 +8,8 @@ import os
 
 #------------------
 # Options: 
-splitchar = '-'
-extensions = [] # Types of files you want to rename (Whitelist). Can be empty. -- ["json", "txt"]
+splitchar = '_'
+extensions = ["aac"] # Types of files you want to rename (Whitelist). Can be empty. -- ["mp4", "aac"]
 test = True # Don't actully do anything just yet. Set to False when you are certain of the results.
 #------------------
 
@@ -17,16 +17,13 @@ for filename in os.listdir('.\\'):
     if not filename == os.path.basename(__file__):
         extension = filename.split(".")[-1:] # Get filetype
         if (not extensions) or (extension[0] in extensions):
-            newFilename = ""
-            parts = "".join(filename.split(".")[0:-1]).split(splitchar) # Remove file type and split
-            for i in range(len(parts),0,-1):
-                newFilename += parts[i-1]+splitchar
-            newFilename = "".join(newFilename.rsplit(splitchar, 1))
-            newFilename += '.'+extension[0]
+            number = filename.split(splitchar)[-1:].split(".")[0] # Gets "0", "1"... and removes extension.
+            if not os.path.exists(number):
+                os.makedirs(number)
+            newFilename = number + "\\" + filename
             
             if not test:
-                os.rename(filename, newFilename)
+                os.move(filename, newFilename)
             print(filename + " -> " + newFilename)
-    
 if test:
     print("\nHappy with the results? Want it to actually replace the file names? Change test from True to False.")
