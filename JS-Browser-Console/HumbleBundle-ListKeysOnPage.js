87dace6519bfe291c1d0021399c8fb7acefe922e
Wesley Pyburn (TechNobo) <technobo@tcno.co>
2020-06-23 11:11:42 +0200

    Create HumbleBundle-ListKeysOnPage.js

---
+++ JS-Browser-Console/HumbleBundle-ListKeysOnPage.js
@@ -0,0 +1,13 @@
+// Want to grab all the keys on a given page, and list them so you can either save them, send them or use them in a program like SteamBulkActivator?
+// This project collects all keys from the page (with the CSS class ".keyfield-value") and lists them, for you to copy from your console.
+//
+// Created by Wesley Pyburn (TechNobo)
+// https://github.com/TcNobo/TcNo-Random-Scripts
+// Based on: https://stackoverflow.com/questions/22997516/to-find-all-emails-on-a-page/22997761
+var ikws = true;  // IgnoreKeysWithSpaces?
+var list = ""; var tot = 0;
+Array.prototype.slice.call(document.getElementsByClassName("keyfield-value")).forEach((item, index) => {
+  if (!ikws || !item.innerHTML.includes(" ")){list += item.innerHTML + "\n"; tot++;}
+});
+console.log(list);
+console.log("Total number of keys on page: " + tot);
