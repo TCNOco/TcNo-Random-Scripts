28f1fedb08aa3c768cdcda1d62c0641502e19218
Wesley Pyburn (TechNobo) <10319195+TcNobo@users.noreply.github.com>
2021-03-06 22:06:47 +0200

    Create PNG_to_WEBP.cmd

---
+++ CMD/PNG_to_WEBP.cmd
@@ -0,0 +1,22 @@
+@echo off &setlocal enabledelayedexpansion
+
+REM Just a simple PNG to WEBP converter using cwebp.
+REM Add cwebp to PATH for this to work, or define the full path.
+REM Created March 2021
+
+REM Created by Wesley Pyburn (TechNobo)
+REM https://github.com/TcNobo/TcNo-Random-Scripts
+
+set outfolder=webp\
+set quality=75
+mkdir !outfolder!
+
+for %%v in ("*.png" "*.jpg" "*.jpeg") do (
+  set FileName=%%v
+  set name=!FileName:~0,-4!
+  set output="!outfolder!!name!.webp"
+
+  echo Converting: !FileName! into !output!
+  cwebp -q !quality! !FileName! -o !output!
+)
+pause
