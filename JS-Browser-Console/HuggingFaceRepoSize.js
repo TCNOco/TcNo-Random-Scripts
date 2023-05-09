45ec840e96c7f590477b97bd1eb773d7db93cb20
Wesley Pyburn <10319195+TCNOco@users.noreply.github.com>
2023-05-09 19:36:53 +0200

    2023-05-09 - Added HF Repo Size Calc

---
+++ JS-Browser-Console/HuggingFaceRepoSize.js
@@ -0,0 +1,60 @@
+// It's difficult to find the total size of a repo on HuggingFace when there are tons of files.
+// This script calculates the total file size of all download links with the "title" attribute set to "Download file"
+// on the current webpage, and logs the result to the console in a human-readable format.
+//
+// Created by Wesley Pyburn (TechNobo)
+// https://github.com/TcNobo/TcNo-Random-Scripts
+
+// Initialize total size variable to 0
+let totalSize = 0;
+
+// Get all download links with the "title" attribute set to "Download file"
+const downloadLinks = document.querySelectorAll('a[title="Download file"]');
+
+// Loop through each download link and extract its size information
+downloadLinks.forEach(link => {
+  // Get the size text from the link's inner text
+  const sizeText = link.innerText.trim();
+
+  // Parse the size value from the size text and convert it to bytes
+  const size = parseFloat(sizeText);
+  const sizeUnit = sizeText.match(/[A-Za-z]+/)[0].toUpperCase();
+  const sizeInBytes = convertToBytes(size, sizeUnit);
+
+  // Add the size in bytes to the total size variable
+  totalSize += sizeInBytes;
+});
+
+// Format the total size in a human-readable format and log it to the console
+console.log(`Total file size: ${bytesToSize(totalSize)}`);
+
+// Helper function to convert a size value and unit to bytes
+function convertToBytes(size, unit) {
+  // Convert the size to bytes based on its unit
+  switch (unit) {
+    case 'KB':
+      return size * 1024;
+    case 'MB':
+      return size * 1024 * 1024;
+    case 'GB':
+      return size * 1024 * 1024 * 1024;
+    default:
+      return size;
+  }
+}
+
+// Helper function to format a size value in a human-readable format
+function bytesToSize(bytes) {
+  // Determine the appropriate unit for the given size value
+  const units = ['B', 'KB', 'MB', 'GB', 'TB'];
+  let size = bytes;
+  let unitIndex = 0;
+
+  while (size > 1024 && unitIndex < units.length - 1) {
+    size /= 1024;
+    unitIndex++;
+  }
+
+  // Return the formatted size value with its appropriate unit
+  return `${size.toFixed(2)} ${units[unitIndex]}`;
+}
--- JS-Browser-Console/SteamDiscoveryQueue.js
+++ JS-Browser-Console/SteamDiscoveryQueue.js
@@ -1,22 +1,31 @@
 // Unfortunately I don't remember the origin of this script, but it's super useful.
 // https://store.steampowered.com/explore/
 
+// Define a function called "Queue" that takes a single argument, x
 var Queue = function(x) {
+    // Log the current value of x to the console
     console.log('Current queue: ' + ++x);
 
+    // Send a POST request to generate a new discovery queue on the Steam store website
     jQuery.post('https://store.steampowered.com/explore/generatenewdiscoveryqueue', {
         sessionid: g_sessionID,
         queuetype: 0
     }).done(function(data) {
+        // When the POST request is successful, create an array to hold multiple POST requests
         var requests = [];
 
+        // Loop through each item in the queue returned by the POST request
         for (var i = 0; i < data.queue.length; i++) {
+            // Create a new POST request for each item in the queue, to remove it from the queue
             requests.push(jQuery.post('https://store.steampowered.com/app/10', {
                 appid_to_clear_from_queue: data.queue[i],
                 sessionid: g_sessionID
             }));
         }
 
+        // When all of the POST requests have completed, check if we've reached the maximum number of iterations (3)
+        // If not, call the Queue function again with an incremented value of x
+        // If we have reached the maximum number of iterations, reload the page
         jQuery.when.apply(jQuery, requests).done(function() {
             if (x < 3) {
                 Queue(x);
@@ -27,4 +36,5 @@ var Queue = function(x) {
     });
 };
 
+// Call the Queue function with an initial value of 0 to start the process
 Queue(0);
