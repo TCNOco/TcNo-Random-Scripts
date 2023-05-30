80005c46306c89cd7ac90ca8eb9ecc3a74946020
Wesley Pyburn <10319195+TCNOco@users.noreply.github.com>
2023-05-30 14:17:55 +0200

    2023-05-30- RemBG

---
+++ CMD/RemoveBG.bat
@@ -0,0 +1,23 @@
+@REM 1
+@echo off
+setlocal
+
+REM A simple batch file to interact with 'rembg'.
+REM Drag and drop a file onto this bat file to process it and remove the background.
+
+REM Created by Wesley Pyburn (TechNobo)
+REM https://github.com/TcNobo/TcNo-Random-Scripts
+
+
+if "%~1"=="" (
+  echo Drag an image file onto this batch file to process it.
+  pause
+  goto :EOF
+)
+
+set "input=%~1"
+set "output=%~dp1%~n1_r.png"
+
+rembg i "%input%" "%output%"
+
+echo Processing complete: "%output%"
